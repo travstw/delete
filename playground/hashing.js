41eeaff3b60b5b674c1df48fa7c2378a625c4d43
travstw <travstw@gmail.com>
2018-01-11 13:46:41 -0800

    tokens

---
+++ playground/hashing.js
@@ -0,0 +1,15 @@
+const {SHA256} = require('crypto-js');
+
+const jwt = require('jsonwebtoken');
+
+let data = {
+    id: 10
+};
+
+let token = jwt.sign(data, '123abc');
+//console.log(token);
+
+
+let decoded = jwt.verify(token, '123abc');
+
+console.log(decoded);
--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,57 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-const User = mongoose.model('User', {
+const UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
         trim: true,
-        minlength: 1
-    }
-});
+        minlength: 1, 
+        unique: true,
+        validate: {
+            isAsync: true,
+            validator: validator.isEmail, 
+            message: '{VALUE} is not a valid email'
+        }
+    }, 
+    password: {
+        type: String,
+        require: true,
+        minlength: 6
+    }, 
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
+})
 
+UserSchema.methods.generateAuthToken = function () {
+    let user = this;
 
-module.exports = {User};
+    let access = 'auth';
+    let token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123')
+    user.tokens.push({access, token});
+
+    return user.save().then(() => {
+        return token;
+    })
+}
+
+UserSchema.methods.toJSON = function () {
+    let user = this;
+    let userObject = user.toObject();
+
+    return _.pick(userObject, ['_id', 'email']);
+};
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = {User}
